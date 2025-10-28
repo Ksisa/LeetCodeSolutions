a4987407fca4d97b6ace858b95c72898c2d2a46c
Kris <Kris@DESKTOP-C1BKPIV>
2025-10-28 18:37:49 +0000

    Add project files.

---
+++ Solution1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // Forgot
+    public class Solution1
+    {
+        public int NumberOfBeams(string[] bank)
+        {
+            if (bank.Length == 0) return 0;
+
+            var sum = 0;
+            var prevRowSecurityDeviceCount = 0;
+
+            // If the string is all 0's, can ignore
+            foreach (string floor in bank)
+            {
+                var securityDevices = CountSecurityDevices(floor);
+                if (securityDevices != 0)
+                {
+                    sum += prevRowSecurityDeviceCount * securityDevices;
+                    prevRowSecurityDeviceCount = securityDevices;
+                }
+            }
+
+            return sum;
+        }
+
+        private int CountSecurityDevices(string row) => row.Count(i => i == '1');
+    }
+}
---
+++ Solution2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /*
+    You are given two positive integers n and limit.
+
+    Return the total number of ways to distribute n candies among 3 
+    children such that no child gets more than limit candies.
+     
+     */
+    // 2929. Distribute Candies Among Children II
+
+    public class Solution2
+    {
+        public long DistributeCandies(int n, int limit)
+        {
+            int ways = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+            }
+        }
+    }
+}
